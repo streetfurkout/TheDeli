333a4004a479a09c3471a3be4d3e08c77044bfa4
Furkan <furkanamerikaa@gmail.com>
2024-11-08 15:00:07 -0500

    Get price method added to the meat class

--- TheDeli/src/main/java/com/pluralsight/Meat.java
+++ TheDeli/src/main/java/com/pluralsight/Meat.java
@@ -1,5 +1,13 @@
 package com.pluralsight;
 
 public class Meat extends Topping{
+    public Meat(String type) {
+        super(type);
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
 
 }
