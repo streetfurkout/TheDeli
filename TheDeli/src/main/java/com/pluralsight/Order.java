b98707e028eb9274c7d4536614bbf4e6761a3f69
Furkan <furkanamerikaa@gmail.com>
2024-11-11 19:45:37 -0500

    chip, drink and sandwich methods added to the order list

--- TheDeli/src/main/java/com/pluralsight/Order.java
+++ TheDeli/src/main/java/com/pluralsight/Order.java
@@ -10,4 +10,15 @@ public class Order {
         drinks = new ArrayList<>();
         chips = new ArrayList<>();
     }
+    public void addSandwich(Sandwich sandwich) {
+        sandwiches.add(sandwich);
+    }
+
+    public void addDrink(Drink drink) {
+        drinks.add(drink);
+    }
+
+    public void addChips(Chip chips) {
+        this.chips.add(chips);
+    }
 }
