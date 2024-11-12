e33c81baec6bb9dba00824facb2dc01d3c6c3ba3
Furkan <furkanamerikaa@gmail.com>
2024-11-12 09:37:20 -0500

    Sandwich class created, attirubutes and constructors added

--- TheDeli/src/main/java/com/pluralsight/Sandwich.java
+++ TheDeli/src/main/java/com/pluralsight/Sandwich.java
@@ -1,4 +1,16 @@
 package com.pluralsight;
 
+import java.util.List;
+
 public class Sandwich {
-}
+    private String size;
+    private String breadType;
+    private boolean toasted;
+    private List<Topping> toppings;
+
+    public Sandwich(String size, String breadType, boolean toasted, List<Topping> toppings) {
+        this.size = size;
+        this.breadType = breadType;
+        this.toasted = toasted;
+        this.toppings = toppings;
+    }
