487ed07a3355eeada80c2b1453c43342b6a6a225
Furkan <furkanamerikaa@gmail.com>
2024-11-12 09:44:07 -0500

    calculatePrice method created at the Sandwich class

--- TheDeli/src/main/java/com/pluralsight/Sandwich.java
+++ TheDeli/src/main/java/com/pluralsight/Sandwich.java
@@ -14,3 +14,21 @@ public class Sandwich {
         this.toasted = toasted;
         this.toppings = toppings;
     }
+    public double calculatePrice() {
+        double basePrice = 0;
+
+        if (size.equals("4")) {
+            basePrice = 5.50;
+        } else if (size.equals("8")) {
+            basePrice = 7.00;
+        } else if (size.equals("12")) {
+            basePrice = 8.50;
+        }
+
+        double toppingsCost = 0;
+        for (Topping topping : toppings) {
+            toppingsCost += topping.getPrice();
+        }
+
+        return basePrice + toppingsCost;
+    }
