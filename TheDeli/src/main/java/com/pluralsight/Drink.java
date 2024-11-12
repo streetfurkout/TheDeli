f894e1136b75353463fbbe6a60baa19ff9cbc66f
Furkan <furkanamerikaa@gmail.com>
2024-11-11 19:22:40 -0500

    Drink class size options added

--- TheDeli/src/main/java/com/pluralsight/Drink.java
+++ TheDeli/src/main/java/com/pluralsight/Drink.java
@@ -3,4 +3,12 @@ package com.pluralsight;
 public class Drink {
     private String size;
     private double price;
+
+    public Drink(String size) {
+        this.size = size;
+        if(size.equalsIgnoreCase("small")) {this.price = 2.00;}
+        else if (size.equalsIgnoreCase("medium")){this.price=2.50;}
+        else if (size.equalsIgnoreCase("large")) {this.price=3.00;}
+    }
+    }
 }
