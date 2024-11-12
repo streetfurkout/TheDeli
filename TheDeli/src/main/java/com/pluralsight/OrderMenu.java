2b88eb6033480ee66785255d62be4d81e4bd594e
Furkan <furkanamerikaa@gmail.com>
2024-11-11 22:04:22 -0500

    Adding chips/sandwich/drink methods created and checkout method created at the OrderMenu class

--- TheDeli/src/main/java/com/pluralsight/OrderMenu.java
+++ TheDeli/src/main/java/com/pluralsight/OrderMenu.java
@@ -14,15 +14,16 @@ public class OrderMenu {
         boolean ordering = true;
 
         while (ordering) {
-            System.out.println("\n--- Menu ---");
+            System.out.println("\n--- Order Menu ---");
             System.out.println("1) Add Sandwich");
-            System.out.println("2) Add drink");
-            System.out.println("3) Add chips");
+            System.out.println("2) Add Drink");
+            System.out.println("3) Add Chips");
             System.out.println("4) Checkout");
-            System.out.println("0) Cancel the order");
-            System.out.print("Your choice is: ");
+            System.out.println("0) Cancel Order");
+            System.out.print("Your Order: ");
             int choice = scanner.nextInt();
             scanner.nextLine();
+
             if (choice == 1) {
                 addSandwich();
             } else if (choice == 2) {
@@ -33,10 +34,41 @@ public class OrderMenu {
                 checkout();
                 ordering = false;
             } else if (choice == 0) {
-                System.out.println("Order Canceled.");
+                System.out.println("Order Created.");
                 ordering = false;
             } else {
-                System.out.println("Invalid.");
+                System.out.println("Invalid Option!! Please Try Again.");
             }
         }
     }
+
+    private void addSandwich() {
+        SandwichBuilder sandwichBuilder = new SandwichBuilder();
+        Sandwich sandwich = sandwichBuilder.buildSandwich();
+        order.addSandwich(sandwich);
+        System.out.println("Sandwich Added.");
+    }
+
+    private void addDrink() {
+        DrinkBuilding drinkBuilder = new DrinkBuilding();
+        Drink drink = drinkBuilder.buildingDrink();
+        order.addDrink(drink);
+        System.out.println("Drink Added.");
+    }
+
+    private void addChips() {
+        Chips chips = new Chips();
+        order.addChips(chips);
+        System.out.println("Chips Added.");
+    }
+
+    private void checkout() {
+        System.out.println("\n--- Your Order ---");
+        System.out.println(order);
+        System.out.println("Total Amount: $" + order.calculateTotal());
+
+        // Save the order to a receipt file
+        Receipt.saveOrder(order);
+        System.out.println("Order Created and Receipt Saved.");
+    }
+}
