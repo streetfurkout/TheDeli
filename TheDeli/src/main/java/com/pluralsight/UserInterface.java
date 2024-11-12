f2c54ec125f73fbdf6c21d25d46a2036d4b3984b
Furkan <furkanamerikaa@gmail.com>
2024-11-12 16:29:30 -0500

    userInterface Class addChips method changed

--- TheDeli/src/main/java/com/pluralsight/UserInterface.java
+++ TheDeli/src/main/java/com/pluralsight/UserInterface.java
@@ -1,13 +1,80 @@
 package com.pluralsight;
 
+import com.pluralsight.orderedService.Chips;
 import com.pluralsight.orderedService.Drink;
+import com.pluralsight.orderedService.Sandwich;
 
 import java.util.Scanner;
 
 public class UserInterface {
+    private Order order;
+    private Scanner scanner = new Scanner(System.in);
+
+    public UserInterface(Order order) {
+        this.order = order;
+    }
 
 
-    Scanner scanner = new Scanner(System.in);
+    public void homeScreen(){
+    boolean running = true; {
+        while (running)
+        System.out.println("---- DELI Order Menu Screen----");
+        System.out.println("1) New Order");
+        System.out.println("0) Exit");
+        System.out.print("Your Choice: ");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        if (choice == 1) {
+            displayOrderMenu();
+        } else if (choice == 0) {
+            System.out.println("Program Ending...");
+            running = false;
+        } else {
+            System.out.println("Invalid Option!! Please Try Again.");
+        }
+    }
+    }
+
+    public void displayOrderMenu() {
+        boolean ordering = true;
+
+        while (ordering) {
+            System.out.println("\n--- Order Menu ---");
+            System.out.println("1) Add Sandwich");
+            System.out.println("2) Add Drink");
+            System.out.println("3) Add Chips");
+            System.out.println("4) Checkout");
+            System.out.println("0) Cancel Order");
+            System.out.print("Your Order: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (choice == 1) {
+                addSandwich();
+            } else if (choice == 2) {
+                addDrink();
+            } else if (choice == 3) {
+                addChips();
+            } else if (choice == 4) {
+                checkout();
+                ordering = false;
+            } else if (choice == 0) {
+                System.out.println("Order Created.");
+                ordering = false;
+            } else {
+                System.out.println("Invalid Option!! Please Try Again.");
+            }
+        }
+    }
+
+    private void addSandwich() {
+        SandwichBuilder sandwichBuilder = new SandwichBuilder();
+        Sandwich sandwich = sandwichBuilder.buildSandwich();
+        order.addSandwich(sandwich);
+        System.out.println("Sandwich Added.");
+    }
+
     public Drink addDrink() {
 
         System.out.println("\n--- Drink Building ---");
@@ -42,5 +109,36 @@ public class UserInterface {
 
         }
         return new Drink(size, selection);
+    }
+
+    private void addChips() {
+
+        System.out.println("Please select a flavour.");
+        System.out.println("1- Salty Pepper \n" +
+                "2- Ranch Doritos \n");
+        String flavour = scanner.nextLine();
+        String selection = null;
+        switch (flavour) {
+            case "1":
+                selection = "Salty Pepper";
+                break;
+            case "2":
+                selection = "Ranch Doritos";
+                break;
+            default:
+                System.out.println("Invalid Option!");
+
+        }   order.addChips(new Chips(selection));
+
+    }
+
+    private void checkout() {
+        System.out.println("\n--- Your Order ---");
+        System.out.println(order);
+        System.out.println("Total Amount: $" + order.calculateTotal());
+
+        // Save the order to a receipt file
+        Receipt.saveOrder(order);
+        System.out.println("Order Created and Receipt Saved.");
     }
 }
